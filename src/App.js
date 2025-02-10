bd784d8546c5fe7d7a14127cd3cd26e0f6ede5d3
Tanmay-Kataria <144655348+Tanmay-Kataria@users.noreply.github.com>
2025-02-11 00:17:48 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,60 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import {  collection, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore'; 
-import { db } from './firebase';
-import AddQuestionForm from './AddQuestionForm';
-import FilterBar from './FilterBar';
-import ReorderableList from './ReorderableList';
-import './output.css';
+import Header from './header';
+import './App.css';
+import Image from './image';
+import { Theme } from '@radix-ui/themes';
+import Itemlist from './Itemlist';
+import Cardlist from './Cardlist';
+import Subscribe from './subscribe';
+import Footer from './footer';
 
-const App = () => {
-  const [questions, setQuestions] = useState([]);
-  const [filterTitle, setFilterTitle] = useState('');
-  const [filterTag, setFilterTag] = useState('');
-  const [filterDate, setFilterDate] = useState('');
-
-  useEffect(() => {
-    const fetchQuestions = async () => {
-      const querySnapshot = await getDocs(collection(db, 'questions'));
-      const questionList = querySnapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setQuestions(questionList);
-    };
-
-    fetchQuestions();
-  }, []);
-
-  const handleAddQuestion = async (newQuestion) => {
-    const docRef = await addDoc(collection(db, 'questions'), newQuestion);
-    setQuestions([...questions, { id: docRef.id, ...newQuestion }]);
-  };
-
-  const handleDeleteQuestion = async (id) => {
-    await deleteDoc(doc(db, 'questions', id));
-    setQuestions(questions.filter((question) => question.id !== id));
-  };
-
-  const filteredQuestions = questions.filter((q) =>
-    (!filterTitle || q.title.toLowerCase().includes(filterTitle.toLowerCase())) &&
-    (!filterTag || q.tag.toLowerCase().includes(filterTag.toLowerCase())) &&
-    (!filterDate || q.date === filterDate)
-  );
-
+function App() {
   return (
-    <div className="container mx-auto p-4">
-      <AddQuestionForm onAdd={handleAddQuestion} />
-      <FilterBar
-        filterTitle={filterTitle}
-        setFilterTitle={setFilterTitle}
-        filterTag={filterTag}
-        setFilterTag={setFilterTag}
-        filterDate={filterDate}
-        setFilterDate={setFilterDate}
-      />
-      <ReorderableList questions={filteredQuestions} onDelete={handleDeleteQuestion} />
+    <div>
+      <Theme accentColor="gray" grayColor="sage" scaling="100%" appearance='dark'>
+        <div><Header /></div>
+        <Image />
+        <Cardlist />
+        <Itemlist />
+        <Subscribe />
+        <Footer />
+      </Theme>
     </div>
   );
-};
+}
 
 export default App;
