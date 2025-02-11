238bbad811ab5da8d1f45a9aeba61fbd385d5b13
Tanmay Kataria <tanmayabhinav@gmail.com>
2025-02-11 10:41:32 +0530

    updated project

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <center>
+        <h1>Count: {count}</h1>
+        <button
+          onClick={() => setCount(count + 1)}
+          style={{
+            marginRight: "10px",
+            color: "blue",
+            width: "100px",
+            height: "50px",
+          }}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => setCount(count - 1)}
+          style={{ color: "red", width: "100px", height: "50px" }}
+        >
+          Decrement
+        </button>
+      </center>
+    </div>
+  );
+}
+
+function DisplayName() {
+  const  [name, setName]  = useState('');
+  const [submittedName, setSubmittedName]  = useState('');
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmittedName(name);
+  };
+
+  const handleReset = () => {
+    setName("");
+    setSubmittedName("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={handleChange}
+          placeholder="Enter your name"
+        />
+        <button type="submit">Submit</button>
+        <button type="button">Reset</button>
+      </form>
+
+      <h1>
+        {submittedName} . Hello {name}
+      </h1>
+    </div>
+  );
+}
+
+function App() {
+  return <DisplayName></DisplayName>;
+}
+
+export default App;
